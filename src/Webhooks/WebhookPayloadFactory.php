f06de80979e2117940c36ff2b922e09dcbf73463
Manuel Christlieb <manuel@christlieb.eu>
2026-07-03 20:10:48 +0200

    fix: tighten webhook dispatch contracts

--- src/Webhooks/WebhookPayloadFactory.php
+++ src/Webhooks/WebhookPayloadFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Bambamboole\Spectacular\Webhooks;
 
 use Illuminate\Support\Str;
+use ReflectionClass;
 use RuntimeException;
 
 final readonly class WebhookPayloadFactory
@@ -12,12 +13,23 @@ final readonly class WebhookPayloadFactory
     {
         $payloadMethod = $definition->attribute->payloadMethod;
         $eventClass = $event::class;
+        $reflection = new ReflectionClass($event);
 
-        if (! is_callable([$event, $payloadMethod])) {
+        if (! $reflection->hasMethod($payloadMethod)) {
             throw new RuntimeException("Webhook payload method [{$payloadMethod}] is missing on [{$eventClass}]");
         }
 
-        $data = $event->{$payloadMethod}();
+        $method = $reflection->getMethod($payloadMethod);
+
+        if (! $method->isPublic()) {
+            throw new RuntimeException("Webhook payload method [{$payloadMethod}] on [{$eventClass}] must be public");
+        }
+
+        if ($method->getNumberOfRequiredParameters() > 0) {
+            throw new RuntimeException("Webhook payload method [{$payloadMethod}] on [{$eventClass}] must have zero required parameters");
+        }
+
+        $data = $method->invoke($event);
 
         if (! is_array($data)) {
             throw new RuntimeException("Webhook payload method [{$payloadMethod}] on [{$eventClass}] must return an array");
