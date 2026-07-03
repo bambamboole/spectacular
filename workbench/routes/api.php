16cf3d4b32aa8689bf17dd4316b27dee622149e2
Manuel Christlieb <manuel@christlieb.eu>
2026-07-03 22:35:31 +0200

    feat: add a POST /users endpoint with a request body to the workbench

--- workbench/routes/api.php
+++ workbench/routes/api.php
@@ -5,9 +5,11 @@ use Illuminate\Support\Facades\Route;
 use Workbench\App\Http\Controllers\CategoriesController;
 use Workbench\App\Http\Controllers\RolesController;
 use Workbench\App\Http\Controllers\ShowUserController;
+use Workbench\App\Http\Controllers\StoreUserController;
 use Workbench\App\Http\Controllers\UsersController;
 
 Route::get('api/roles', RolesController::class)->name('api.roles.index');
 Route::get('api/users', UsersController::class)->name('api.users.index');
+Route::post('api/users', StoreUserController::class)->name('api.users.store');
 Route::get('api/users/{user}', ShowUserController::class)->name('api.users.show');
 Route::get('api/categories', CategoriesController::class)->name('api.categories.index');
